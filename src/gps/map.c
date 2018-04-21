3ab171b894694b2b409f271bc24bf6e0618734e1
xavierefremont <xaviere.fremont@yahoo.fr>
2018-04-21 21:28:45 +0200

    structure map

--- src/gps/map.c
+++ src/gps/map.c
@@ -1,19 +1,4 @@
-//
-// Created by julien on 18/04/18.
-//
-
-#ifndef __MAP_H
-#define __MAP_H
-
-typedef struct Position {
-  int x;
-  int y;
-}Position;
-
-typedef struct Map {
-  Position size;
-  char** plan;
-}Map;
+#include "../../include/map.h"
 
 /**
  * Creates a structure map initializing the size sent by the driver
@@ -21,26 +6,32 @@ typedef struct Map {
  * @param height of the map
  * @return the new map
  */
-Map createMap(int width, int heigth);
+Map createMap(int width, int heigth) {
+  return NULL;
+}
 
 /**
  * Initializes the map, i.e fill the plan in
  * @param map pointer to initialize
  */
-void initMap(struct Map*);
+void initMap(struct Map*) {
+
+}
 
 /**
  * Set int the map the current position of the three cars
  * @param car position ouf ourselves
  * @param rival1, rival2 position of the others
  */
-void setPosition(Position car, Position rival1, Position rival2);
+void setPosition(Position car, Position rival1, Position rival2) {
+
+}
 
 /**
  * Send to the GPS the structure of the map changed to estimate
  * speed and acceleration
  * @param the new map
  */
-void sendToGPS(struct Map);
+void sendToGPS(struct Map) {
 
-#endif //__MAP_H
+}
